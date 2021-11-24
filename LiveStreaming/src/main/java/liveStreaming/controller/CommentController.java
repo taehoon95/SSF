a32bfbedc93917c74ed73958a5edf20b975353e5
kangdongha97 <kangdongha97>
2021-11-24 12:19:28 +0900

    2021-11-24 강동하 댓글기능 완료

--- LiveStreaming/src/main/java/liveStreaming/controller/CommentController.java
+++ LiveStreaming/src/main/java/liveStreaming/controller/CommentController.java
@@ -1,12 +1,15 @@
 package liveStreaming.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import liveStreaming.service.CommentService;
 import liveStreaming.dto.CommentDto;
 
@@ -30,5 +33,22 @@ public class CommentController {
 		System.out.println("===================");
 		System.out.println(v_code);
 		return ResponseEntity.ok(service.commentSelect(v_code));
+	}
+
+    // 2021-11-24 강동하 댓글 update
+	@PatchMapping("/commentupdate")
+	public ResponseEntity<Object> commentUpdate(@RequestBody CommentDto comment){
+		System.out.println("===================");
+		System.out.println(comment.getM_num());
+		System.out.println(comment.getM_text());
+		return ResponseEntity.ok(service.commentUpdate(comment));
+	}
+
+    // 2021-11-23 강동하 댓글 delete
+	@DeleteMapping("/commentdelete")
+	public ResponseEntity<Object> commentDelete(@RequestBody CommentDto comment){
+		System.out.println("===================");
+		System.out.println(comment.getM_num());
+		return ResponseEntity.ok(service.commentDelete(comment));
 	}
 }
